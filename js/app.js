c0c04a63b4dd3be0cd33c1584e8b90dc3adbec7e
Carlos Meregildo <carlos.delacruz228@gmail.com>
2021-05-20 18:53:29 -0400

    Disabled button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('starting...');
+const sendBtn = document.querySelector('#send');
+
+
+
+eventListeners();
+function eventListeners(){
+    document.addEventListener('DOMContentLoaded', AppInit);
+}
+
+function AppInit(){
+    sendBtn.disabled = true;
+}
