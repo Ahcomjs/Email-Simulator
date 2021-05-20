48f6406dd3b06d4db4dff96d9956eaf937d5febe
Carlos Meregildo <carlos.delacruz228@gmail.com>
2021-05-20 19:55:55 -0400

    Email validation

--- js/app.js
+++ js/app.js
@@ -1,12 +1,38 @@
 const sendBtn = document.querySelector('#send');
 
+const email = document.querySelector('#email');
+const subject = document.querySelector('#subject');
+const message = document.querySelector('#message');
 
 
 eventListeners();
 function eventListeners(){
     document.addEventListener('DOMContentLoaded', AppInit);
+    
+    email.addEventListener('blur', formValidate);
+    subject.addEventListener('blur', formValidate);
+    message.addEventListener('blur', formValidate);
+    
+
 }
 
 function AppInit(){
     sendBtn.disabled = true;
+}
+
+
+function formValidate(event){
+
+    if(event.target.value.length > 0){
+        console.log('Testing');
+    }else{
+        vent.target.classList.add('error');
+        showError();
+    }
+   
+}
+
+function showError(){
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = 'All Fields are required';
 }
