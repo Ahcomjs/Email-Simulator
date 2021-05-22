a39469dcaff1d7bdbbe4ae0b1188a3a34ccf03ad
Carlos Meregildo <carlos.delacruz228@gmail.com>
2021-05-22 01:43:09 -0400

    Clean error message

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const sendBtn = document.querySelector('#send');
+const form = document.querySelector('#send-mail');
 
 const email = document.querySelector('#email');
 const subject = document.querySelector('#subject');
@@ -26,7 +27,7 @@ function formValidate(event){
     if(event.target.value.length > 0){
         console.log('Testing');
     }else{
-        vent.target.classList.add('error');
+        event.target.style.borderBottom = '2px solid red';
         showError();
     }
    
@@ -35,4 +36,11 @@ function formValidate(event){
 function showError(){
     const errorMessage = document.createElement('p');
     errorMessage.textContent = 'All Fields are required';
+    errorMessage.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mb-5', 'text-center','error');
+
+    const errors = document.querySelectorAll('.error');
+    if(errors.length === 0){
+        form.insertBefore(errorMessage, document.querySelector('.mb-10'))
+    }
+
 }
